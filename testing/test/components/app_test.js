d452d7d0f7b2521f7e10eedbf87d857b6f3402df
Heather Park <hthr.prk@gmail.com>
2018-05-09 10:16:36 -0700

    Write first test

--- testing/test/components/app_test.js
+++ testing/test/components/app_test.js
@@ -1,19 +1,10 @@
 import { renderComponent, expect } from '../test_helper';
 import App from '../../src/components/app';
 
-// Use 'describe' to group together similar tests
 describe('App', () => {
-  let component;
-
-  beforeEach(() => {
-    component = renderComponent(App);
-  });
-
-  it('shows a comment box', () => {
-    expect(component.find('.comment-box')).to.exist;
-  });
-
-  it('shows a comment list', () => {
-    expect(component.find('.comment-list')).to.exist;
+  it('shows the correct text', () => {
+    const component = renderComponent(App);
+    
+    expect(component).to.contain('React simple starter');
   });
 });
